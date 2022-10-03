e29660687b461a4626c9420d0110f7572e4d9184
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-03 23:25:34 +0000

    Adding numeric_owner as keyword arguement

--- scripts/get_rust_pkg.py
+++ scripts/get_rust_pkg.py
@@ -162,7 +162,7 @@ def fetch_pkg(args, pkg, dl_path):
             if not is_within_directory(path, member_path):
                 raise Exception("Attempted Path Traversal in Tar File")
     
-        tar.extractall(path, members, numeric_owner) 
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
         
     
     safe_extract(tfile, tmp_dir)
